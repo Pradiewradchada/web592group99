82bf88b8bdbf2e38db0acddf47b84de731933b08
Wachirawut Thamviset <twachikku@gmail.com>
2017-04-04 07:31:50 +0700

    add bootstrap new-age template

--- main.php
+++ main.php
@@ -1,10 +1,3 @@
-﻿<html>
- <head>
-   <meta charset="utf-8"/>
-   <title>โครงงานกลุ่ม 99</title>
- </head>
-<body>
- <h1>โครงงานกลุ่ม 99</h1>
- ทดสอบแก้ไขไฟล์ 
-</body>
-</html>
+﻿<?php
+  include("index.html");
+?>

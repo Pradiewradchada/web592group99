787d7b4abba009d084db43136e60f40ba3157b8a
Wachirawut Thamviset <twachikku@gmail.com>
2017-04-04 07:57:11 +0700

    Update main.php

--- main.php
+++ main.php
@@ -1,9 +1,6 @@
 <?php
+  echo "TEST";
   include("index.html");
-<<<<<<< HEAD
-  echo "Hello";
+  echo "TEST Edit File ";    
+  
 ?>
-=======
-  echo "TEST Edit File";    
-?>
->>>>>>> d1fef9a0afd8940b705697cec8588cba58402309

70b2417c0e419b44bbc31fa7f6e3c2d814b9c48d
Wachirawut Thamviset <twachikku@gmail.com>
2017-05-02 01:16:37 +0700

    demo

---
+++ menu_admin.php
@@ -0,0 +1,9 @@
+﻿           <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li class="dropdown-header">สำหรับผู้ดูแลระบบ</li>
+                <li><a href="main.php?p=editgroup">Edit Groups</a></li>
+                <li><a href="main.php?p=edit&file=header.txt">Edit Header</a></li>
+                <li><a href="main.php?p=edit&file=footer.txt">Edit Footer</a></li>
+              </ul>
+            </li>
--- web592group99/menu_admin.php
+++
@@ -1,7 +0,0 @@
-<li role="separator" class="divider"></li>
-<li>สำหรับผู้ดูแลระบบ</li>
-<li><a href='main.php?p=edit&file=menu.txt'>Edit Menu</a></li>
-<li><a href='main.php?p=edit&file=products.txt'>Edit Products</a></li>				
-<li><a href='main.php?p=edit&file=header.txt'>Edit Header</a></li>
-<li><a href='main.php?p=edit&file=footer.txt'>Edit Footer</a></li>
-	
